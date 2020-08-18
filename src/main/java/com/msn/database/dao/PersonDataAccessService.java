5afeb5b71d2d68d2d6859828c0ab508fa569931b
MsnUlti <egrsar2000@gmail.com>
2020-08-19 01:30:26 +0400

    Docker and postgres

--- src/main/java/com/msn/database/dao/PersonDataAccessService.java
+++ src/main/java/com/msn/database/dao/PersonDataAccessService.java
@@ -1,6 +1,8 @@
 package com.msn.database.dao;
 
 import com.msn.database.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,28 +11,54 @@ import java.util.UUID;
 
 @Repository("postgres")
 public class PersonDataAccessService implements PersonDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertPerson(UUID id, Person person) {
-        return 0;
+        final String sql = "INSERT INTO person (id, name) VALUES (?, ?)";
+        jdbcTemplate.update(sql, new Object[]{id, person.getName()});
+        return 1;
     }
 
     @Override
     public List<Person> selectAllPeople() {
-        return List.of(new Person(UUID.randomUUID(), "FROM POSTGRES DB"));
+        final String sql = "SELECT id, name FROM person";
+        return jdbcTemplate.query(sql,(resultSet, i) -> {
+            UUID id = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Person(id, name);
+        });
     }
 
     @Override
     public Optional<Person> selectPersonById(UUID id) {
-        return Optional.empty();
+        final String sql = "SELECT id, name FROM person WHERE id = ?";
+         Person person = jdbcTemplate.queryForObject(
+                 sql,
+                 new Object[]{id},
+                 (resultSet, i) -> {
+                    String name = resultSet.getString("name");
+                    return new Person(id, name);
+                });
+         return Optional.ofNullable(person);
     }
 
     @Override
     public int deletePersonById(UUID id) {
-        return 0;
+       final String sql = "DELETE FROM person WHERE id = ?";
+       jdbcTemplate.update(sql, new Object[]{id});
+       return 1;
     }
 
     @Override
     public int updatePersonById(UUID id, Person person) {
-        return 0;
+        final String sql = "UPDATE person SET name = ? WHERE id = ?";
+        jdbcTemplate.update(sql, new Object[]{person.getName(), id});
+        return 1;
     }
 }
---
+++ src/main/java/com/msn/database/datasource/PostgresDatasource.java
@@ -0,0 +1,20 @@
+package com.msn.database.datasource;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PostgresDatasource {
+
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource hikariDataSource(){
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+}
